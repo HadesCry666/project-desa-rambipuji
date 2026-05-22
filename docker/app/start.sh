775f8f2f3316d19bb9021348695f89fa27b14425
Ajiee <tytydak@gmail.com>
2026-05-22 15:47:49 +0700

    setup docker compose and github action deploy

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir -p storage/framework/cache
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/logs
+mkdir -p bootstrap/cache
+
+chmod -R 775 storage bootstrap/cache
+
 php artisan optimize:clear
 php artisan storage:link || true
 
